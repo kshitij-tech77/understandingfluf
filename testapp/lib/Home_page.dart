3685c16decedc59152870a128c50be5b26b806f8
kshitij-tech77 <kshitijdhamala100@gmail.com>
2023-01-24 11:16:26 +0545

    fluf 2

---
+++ testapp/lib/Home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  final int days = 40;
+  final String name = "kshitij";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Kshitijapp")),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+              "I promise I will continue working in fluter for $days  and my name is $name"),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
